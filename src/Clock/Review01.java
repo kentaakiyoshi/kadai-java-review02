36dacad0cf1a956b5a86f2c58c4baf764c2ece98
kentaakiyoshi <kakerulab@gmail.com>
2023-11-13 13:34:43 +0900

    javaの課題を再送します

--- src/Clock/Review01.java
+++ src/Clock/Review01.java
@@ -3,30 +3,14 @@ package Clock;
 public class Review01 {
 
     public static void main(String[] args) {
-        double productPrice = 1000; // 商品価格（例として1000円）
-
-
-        // 消費税の計算
-        double tax = tax(productPrice);
-
-        // 税込価格の計算
-        double totalPrice = productPrice + tax;
+        //価格、税込価格、消費税の金額を変数として定義
+        int productPrice=1000; //商品価格（例：1000円）
+        int taxIncludedPrice=1100; //税込価格（例：1100円）
+        int taxAmount=100; //消費税の金額（例：100円）
 
         // 結果の出力
-        System.out.println("商品の価格：" + productPrice + "円");
-        System.out.println("消費税：" + tax + "円");
-        System.out.println("税込価格：" + totalPrice + "円");
-    }
+        System.out.println(productPrice + "円の商品の税込価格は：" +taxIncludedPrice + "円（消費税は"+taxAmount+"円です）。");
 
-    /**
-     * 商品価格に基づいて消費税を計算するメソッド
-     *
-     * @param price 商品価格
-     * @return 消費税額
-     */
-    public static double tax(double price) {
-        double taxRate = 0.10; // 消費税率（10%）
-        return price * taxRate;
 
     }
 
